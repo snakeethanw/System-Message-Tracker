b37452186fd450bdc9c3f34f5c6ebea6443bdba3
snakeethanw <wongethanj@gmail.com>
2026-02-04 19:21:28 -0800

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -225,8 +225,8 @@ const rest = new REST({ version: "10" }).setToken(TOKEN);
     // Uncomment this block, run once to clear ALL global commands,
     // then comment it out again.
     //
-    // await rest.put(Routes.applicationCommands(CLIENT_ID), { body: [] });
-    // console.log("Cleared ALL global commands.");
+    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: [] });
+    console.log("Cleared ALL global commands.");
 
     // Deploy guild commands
     for (const guildId of GUILDS) {
@@ -248,3 +248,4 @@ const rest = new REST({ version: "10" }).setToken(TOKEN);
     console.error(err);
   }
 })();
+
